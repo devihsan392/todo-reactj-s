e99a4587ac2181d1a82d6ab741b08ca603696cc5
Ihsan Ali <ihsanali12345hhhhh@gmail.com>
2026-07-27 06:25:53 -0700

    add styling and text

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -8,6 +8,8 @@ function Task() {
      const[products, setproducts] = useState([])
   const navigate = useNavigate()
   return (
+    <div>
+
     <div className='main'>
       <div className='main2'>
 
@@ -19,6 +21,20 @@ function Task() {
       </div>
         {/* <button className='btn'  onClick={()=>{navigate("/add-task")}}>Add Task</button> */}
        
+    </div>
+    <div className="hero">
+    <div className='hero2'>
+
+      <h1>Manage Your Daily Tasks with Ease</h1>
+      <p>Never miss an important task again. 
+        Plan your day, stay organized, and boost your
+        productivity effortlessly.</p>
+        <div className='btn-two'>
+          <button className='hero-btn' onClick={()=>{navigate("/task-list")}}>View Work</button>
+          <button className='hero-btn' onClick={()=>{navigate("/add-task")}}>Add Task</button>
+        </div>
+    </div>
+    </div>
     </div>
   )
 }
